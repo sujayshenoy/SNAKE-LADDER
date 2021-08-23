8bee75b7cb8186bbf869d6a829cd3e83f10f7c7e
Sujay S Shenoy <sujay.shenoy@ymedialabs.com>
2021-08-23 22:43:02 +0530

    Method added to process ladder, snake and no-play situations

--- src/com/snakeandladder/Main.java
+++ src/com/snakeandladder/Main.java
@@ -4,9 +4,31 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		final int LADDER = 1;
+		final int SNAKE = 2;
+		final int NO_PLAY = 3;
+		
 		System.out.println("Welcome to Snake and Ladder Game");
 		Game g = new Game();
-		g.rollDie();
+		
+		int dieNumber = g.rollDie();
+		int opt = g.generateOption();
+		
+		switch (opt) {
+		case LADDER:
+			g.pos += dieNumber;
+			System.out.println("Hit Ladder");
+			break;
+		case SNAKE:
+			g.pos -= dieNumber;
+			System.out.println("Hit Snake");
+			break;
+		case NO_PLAY:
+			System.out.println("Hit No-Play");
+			break;
+		}
+		
+		g.printCurrentPosition();
 		
 	}
 
@@ -19,6 +41,15 @@ class Game {
 		int numberOnDie = (int)Math.floor(Math.random()*((6-1)+1)+1);
 		System.out.println("Die Says: "+numberOnDie);
 		return numberOnDie;
+	}
+	
+	public int generateOption() {
+		int option = (int)Math.floor(Math.random()*((3-1)+1)+1);
+		return option;
+	}
+	
+	public void printCurrentPosition() {
+		System.out.println("Position: "+pos);
 	}
 	
 }
