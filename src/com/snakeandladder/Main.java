e16c4c77e52d91c56db6b5319b38d54557a29b52
Sujay S Shenoy <sujay.shenoy@ymedialabs.com>
2021-08-23 22:21:59 +0530

    Player position initialised to 0

--- src/com/snakeandladder/Main.java
+++ src/com/snakeandladder/Main.java
@@ -5,7 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Snake and Ladder Game");
-
+		Game g = new Game();
+		
 	}
 
+}
+
+class Game {
+	int pos = 0;
 }
